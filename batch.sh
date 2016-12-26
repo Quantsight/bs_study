9a243b4142b73f76f12094607c4358c00b8667c9
John Prior <john.w.prior@gmail.com>
2016-12-26 17:45:32 +0000

    exit on error; auto-shutdown after batch complete.

--- batch.sh
+++ batch.sh
@@ -1,11 +1,17 @@
 set -x
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
 # time python eval.py --in_csv ~/scratch/grp1.ds.txt ~/scratch/prj2 --sym_fit LP;
 # time python eval.py ~/scratch/prj2 --grp_fit LP --sym_fit LP;
-time python eval.py ~/scratch/prj2 --sym_fit RF;
-# time python eval.py ~/scratch/prj2 --grp_fit LP;
-time python eval.py ~/scratch/prj2 --grp_fit RF --no_RF_sym;
-time python eval.py ~/scratch/prj2 --grp_fit RF;
-time python eval.py ~/scratch/prj2 --grp_fit LP --sym_fit RF;
-time python eval.py ~/scratch/prj2 --grp_fit RF --sym_fit LP;
-time python eval.py ~/scratch/prj2 --grp_fit RF --sym_fit RF;
+# time python eval.py ~/scratch/prj2 --grp_fit RF;
+time python eval.py ~/scratch/prj2 --grp_fit RF --input_time ; #--criterion mae;
+time python eval.py ~/scratch/prj2 --grp_fit RF --input_extras ; #--criterion mae;
+time python eval.py ~/scratch/prj2 --grp_fit RF --input_time --input_extras ; #--criterion mae;
+
+time python eval.py ~/scratch/prj2 --sym_fit RF --input_time;
+time python eval.py ~/scratch/prj2 --sym_fit RF --input_extras;
+time python eval.py ~/scratch/prj2 --sym_fit RF --input_time --input_extras;
+
+aws ec2 stop-instances --instance-ids i-0e77d63036144e3d8
 
